1b548196c178bc8549e02058c7324744632e90f8
doquan01 <55120808+doquan01@users.noreply.github.com>
2020-10-04 16:33:04 -0700

    Update PalindromeReader

--- Lab 6/PalindromeReader.cpp
+++ Lab 6/PalindromeReader.cpp
@@ -6,21 +6,55 @@
 using namespace std;
 
 void transform( char *raw, char *testStr){
-	
+int i = 0;
+int j = 0;
+int k = 0;
+int count = 0;
+while(*(raw + i) != '\0'){
+	if(isalpha(*(raw + i)) == 0){
+		count++;
+	}
+	i++;
 }
-bool testPalindrome(char *str){
-	
+char* temp = new char[strlen(raw) - count + 1];
+while(*(raw + j) != '\0'){
+	if(isalpha(*(raw + j))){
+		*(temp + k) = toupper(*(raw + j));
+		k++;
+	}
+	j++;
 }
+strcpy(testStr, temp);
+}
+//bool testPalindrome(char *s){
+//    int i = 0;
+//    int r = 1 + strlen(s) - 1;
+//
+//    while(i != r){
+//        if(s[i] != s[r])
+//            return false;
+//        i++;
+//        r--;
+//    }
+//    return true;
+//}
 
 int main(){
-string s;
-getline(cin, s);
-//declares a constant char[] and puts the contents of s into it
-const char* input = s.c_str();
-//make a new char[] of length s + 1 so we can have \0 at the end
-char* test = new char[s.length() + 1];
-//copies char[] input to char[] test
-strcpy(test, input);
+	cout<< "Enter a word or scentence to see if it is a palindrome."<<endl;
+	string s;
+	getline(cin, s);
+
+	//declares a constant char[] and puts the contents of s into it
+	const char* input = s.c_str();
+
+	//make a new char[] of length s + 1 so we can have \0 at the end
+	char* test = new char[s.length() + 1];
+
+	//copies char[] input to char[] test
+	strcpy(test, input);
+	
+	transform(test, test);
+	cout<<test;
 }
 
 
