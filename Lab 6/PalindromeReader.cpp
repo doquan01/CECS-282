9d3216edf2d4982ed5aee6ea32229595a111ba34
doquan01 <55120808+doquan01@users.noreply.github.com>
2020-10-03 17:30:50 -0700

    Added main and methods to PlindromeReader

---
+++ Lab 6/PalindromeReader.cpp
@@ -0,0 +1,26 @@
+//Quan Do
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <cstring>
+using namespace std;
+
+void transform( char *raw, char *testStr){
+	
+}
+bool testPalindrome(char *str){
+	
+}
+
+int main(){
+string s;
+getline(cin, s);
+//declares a constant char[] and puts the contents of s into it
+const char* input = s.c_str();
+//make a new char[] of length s + 1 so we can have \0 at the end
+char* test = new char[s.length() + 1];
+//copies char[] input to char[] test
+strcpy(test, input);
+}
+
+
