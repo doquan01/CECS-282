97dac7df1b4837fd0c4476f6e120936e077474e3
doquan01 <55120808+doquan01@users.noreply.github.com>
2020-10-05 14:05:01 -0700

    Update testPalindrome and transform

--- Lab 6/PalindromeReader.cpp
+++ Lab 6/PalindromeReader.cpp
@@ -6,34 +6,40 @@
 using namespace std;
 
 void transform( char *raw, char *testStr){
-	int j = 0;
-	int count = 0;
+	
+	int count = strlen(raw);
 	for(int i = 0; *(raw+i) != '\0'; i++){
-		if(isalpha(*(raw + i)) == 0){
-			count++;
+		if(isalpha(*(raw+i)) == 0 || isspace(*(raw+i))){
+			count--;
 		}
 	}
-	char* temp = new char[strlen(raw) - count + 1];
-	for(int i = 0; *(raw+i) != '\0'; i++){
-		if(isalpha(*(raw + i))){
-			*(temp + j) = toupper(*(raw + i));
+	char* temp = new char[count+1];
+	int j = 0;
+	for(int i = 0; i < strlen(raw); i++){
+		if(isalpha(*(raw+i))){
+			*(temp+j) = toupper(*(raw+i));
 			j++;
 		}
 	}
+	temp[count] = '\0';
 	strcpy(testStr, temp);
 }
-//bool testPalindrome(char *s){
-//    int i = 0;
-//    int r = 1 + strlen(s) - 1;
-//
-//    while(i != r){
-//        if(s[i] != s[r])
-//            return false;
-//        i++;
-//        r--;
-//    }
-//    return true;
-//}
+bool testPalindrome(char *test){
+    int i = 0;
+    int r = strlen(test) - 1;
+
+    while(i != r){
+        if(*(test+i) != *(test+r)){
+        	 return false;
+		}
+        else{
+			i++;
+        	r--;
+		}
+		return true;
+    }
+    return false;
+}
 
 int main(){
 	cout<< "Enter a word or scentence to see if it is a palindrome."<<endl;
@@ -50,7 +56,12 @@ int main(){
 	strcpy(test, input);
 	
 	transform(test, test);
-	cout<<test;
+	if(testPalindrome(test) == true){
+		cout<<s<<" is a palindrome"<<endl;
+	}
+	else{
+		cout<<s<<" is not a palindrome"<<endl;
+	}
 }
 
 
