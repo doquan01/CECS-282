310c5604f6018273e862a1e26af36a63b0b4342c
doquan01 <55120808+doquan01@users.noreply.github.com>
2020-10-30 14:20:35 -0700

    update 10/30/2020

---
+++ Lab 8/Sales.h
@@ -0,0 +1,21 @@
+#ifndef PUBLICATION_SALES_H
+#define PUBLICATION_SALES_H
+#include <iostream>
+
+using namespace std;
+
+ 
+class Sales {
+
+public:
+//Declare functions getData and putData
+void getData();
+void putData();
+
+//Declare an array to store sales for three months
+protected:
+    float saleArray[3];
+
+};
+
+#endif //PUBLICATION_SALE_H
