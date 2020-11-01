332c79dc8cb64e2328b08e40cbb0e31aeb0660ed
doquan01 <55120808+doquan01@users.noreply.github.com>
2020-11-01 12:27:23 -0800

    update 11/1/2020

---
+++ Lab 8/Main.cpp
@@ -0,0 +1,27 @@
+//Quan Do
+//CECS 282 Lab 8
+#include "Sales.h"
+#include "Publication.h"
+#include "Book.h"
+#include "Digital.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+ 
+
+int main() {
+
+    Book book;
+    Digital digital;
+    book.getData();
+    book.putData();
+    cout <<endl;
+    digital.getData();
+    digital.putData();
+    cout << endl;
+
+    return 0;
+
+}
