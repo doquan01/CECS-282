2f9f937d472aa91aae465580d5bc05921e433b9a
doquan01 <55120808+doquan01@users.noreply.github.com>
2020-11-26 19:08:13 -0800

    update 11/26/2020

---
+++ Lab 10/Alpha.h
@@ -0,0 +1,17 @@
+#ifndef alpha_H
+#define alpha_H
+
+#include <iostream>
+using namespace std;
+
+class alpha
+{
+private:
+int data;
+public:
+alpha();
+alpha(int x);
+void display();
+
+};
+#endif
